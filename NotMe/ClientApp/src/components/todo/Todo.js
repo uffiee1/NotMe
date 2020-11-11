710332cc86a82f652f4af3bf6bc5b647db8ee394
Ufuk Angay <u.angay@gmail.com>
2020-11-11 03:05:45 +0100

    Added Todo structure-WIP

--- NotMe/ClientApp/src/components/todo/Todo.js
+++ NotMe/ClientApp/src/components/todo/Todo.js
@@ -2,10 +2,53 @@
 
 export class Todo extends Component {
     static displayName = Todo.name;
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            items: [],
+            text: ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({text : e.target.value});
+    }
+  
+    handleSubmit(e) {
+        e.preventDefault();
+        var newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState((prevState) => ({
+            items: prevState.items.concat(newItem),
+            text: ''      
+        }));
+    }
 
     render() {
         return (
-        <h1>TODO</h1>
+            <div>
+            <h1>TODO TEST</h1>
+            <TodoList items={this.state.items} />
+            <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} value={this.state.text} />
+            <button>Add</button>
+            </form>
+            </div>
     );
 }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+            {this.props.items.map(item => 
+                (<li key={item.id}>{item.text}</li>))}
+</ul>
+)
+};
 }

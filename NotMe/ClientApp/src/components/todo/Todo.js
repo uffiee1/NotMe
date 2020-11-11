3ad87fdf4325f5934815d6bd1ec1a64389379200
Ufuk Angay <u.angay@gmail.com>
2020-11-11 03:03:51 +0100

    Test Todo page

---
+++ NotMe/ClientApp/src/components/todo/Todo.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export class Todo extends Component {
+    static displayName = Todo.name;
+
+    render() {
+        return (
+        <h1>TODO</h1>
+    );
+}
+}

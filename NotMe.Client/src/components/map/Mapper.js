b264e9fd0c0629e85df61abf5b086b22d5962e67
Ufuk Angay <u.angay@gmail.com>
2021-01-05 16:55:11 +0100

    Changed mapper

--- NotMe.Client/src/components/map/Mapper.js
+++ NotMe.Client/src/components/map/Mapper.js
@@ -1,29 +1,14 @@
-﻿import React, { Component } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-//import './Mapper.css';
+﻿import React, { Component } from "react";
+import { Map, TileLayer, Popup, Marker, Circle } from 'leaflet';
 
-export default class Mapper extends Component {
-    static displayName = Mapper.name;
 
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-    });
+export class Mapper extends Component {
 
     render() {
-        const position = [1.35, 103.8];
         return (
-        <MapContainer
-        className="map"
-        center={position}
-        zoom={10}
-        style={{ height: 500, width: "100%" }}
-    >
-    <TileLayer
-    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        </MapContainer>
+            //TODO: Add Map API
+
+        <h3>TESTING</h3>
     );
-    }
+}
 }

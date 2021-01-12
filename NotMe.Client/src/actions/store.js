38f005a2598e66731fc2f170d2493fd64d0cc8a9
Ufuk Angay <u.angay@gmail.com>
2021-01-12 06:36:10 +0100

    Started with Redux for Todos

--- NotMe.Client/src/actions/store.js
+++ NotMe.Client/src/actions/store.js
@@ -1,12 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { reducers } from "../reducers";
+import { configureStore } from '@reduxjs/toolkit';
+import TodosReducer from './todosReducer';
 
-
-export const store = createStore(
-    reducers,
-    compose(
-        applyMiddleware(thunk),
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+export default configureStore({
+  reducer: {
+    todosReducer: TodosReducer,
+  },
+});

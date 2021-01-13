0b29caa8acf44e6172d46b24180aadfec5cf1bcc
Ufuk Angay <u.angay@gmail.com>
2021-01-13 10:01:24 +0100

    Back-end TodoController done

---
+++ NotMe.BusinessLogic/Services/TodoServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotMe.Models;
+
+namespace NotMe.BusinessLogic.Services
+{
+    public class TodoServices : ITodoServices
+    {
+        public ToDo CreateTodo(ToDo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTodo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditTodo(ToDo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDo GetTodo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDo> GetTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
